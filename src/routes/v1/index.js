1b9f275ea3337e81e02d97d5ccf40710884a85ba
gitakramsm <akram5960@gmail.com>
2026-01-08 21:23:24 +0530

    added new user signup api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const userRouter = require("./user-routes");
 
 const { InfoController } = require("../../controllers");
 
 const router = express.Router();
 
 router.get("/info", InfoController.info);
+router.use("/user", userRouter);
 
 module.exports = router;
