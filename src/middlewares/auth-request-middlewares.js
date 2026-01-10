01d82f9f558414985bd2064bd006dbaaa513dc2b
gitakramsm <akram5960@gmail.com>
2026-01-10 12:08:35 +0530

    added isAuthenticated middleware

--- src/middlewares/auth-request-middlewares.js
+++ src/middlewares/auth-request-middlewares.js
@@ -24,6 +24,21 @@ function validateAuthRequest(req, res, next) {
   next();
 }
 
+async function checkAuth(req, res, next) {
+  try {
+    const response = await UserService.isAuthenticated(
+      req.headers["x-access-token"]
+    );
+    if (response) {
+      req.user = response; // setting the user id in the req object
+      next();
+    }
+  } catch (error) {
+    return res.status(error.statusCode).json(error);
+  }
+}
+
 module.exports = {
   validateAuthRequest,
+  checkAuth,
 };
